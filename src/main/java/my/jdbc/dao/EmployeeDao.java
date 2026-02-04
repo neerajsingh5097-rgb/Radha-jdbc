c103646bd3a21ce4c6e00195f69af9b19e1c1266
Neeraj Singh <neerajsingh5097@gmail.com>
2026-02-04 07:27:31 +0530

    Initial commit

---
+++ src/main/java/my/jdbc/dao/EmployeeDao.java
@@ -0,0 +1,29 @@
+package my.jdbc.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import my.jdbc.model.Employee;
+
+public interface EmployeeDao {
+
+	public void saveEmployee(Employee e)throws SQLException ;
+	
+	public void updateEmployee(Employee e);
+	
+	public void deleteAnEmployee(int id);
+	
+	public void printAllEmployee();
+	
+	public Employee getEmpById(int id);
+	
+	public List<Employee> getAllEmps();
+	
+	public Employee getEmpByName(String name);
+
+
+
+
+
+	
+}
